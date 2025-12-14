412dff314ae2ad37f482e2ea1652dceefb614d24
Pamacea <pamacea@live.fr>
2025-12-14 16:19:55 +0100

    ✅A gear button to the buildBar for a future feature !

--- src/components/Header/BuildBar.tsx
+++ src/components/Header/BuildBar.tsx
@@ -1,10 +1,12 @@
 import { ProfileButton, SkillButton, SphereButton } from "../client/buttons";
+import { GearButton } from "../client/buttons/gear-button";
 
 export const Buildbar = () => {
   return (
     <nav className="w-1/3 h-full flex items-center justify-center">
         <ProfileButton />
         <SkillButton  />
+        <GearButton />
         <SphereButton />
     </nav>
   );
---
+++ src/components/client/buttons/gear-button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const GearButton = () => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      href={`/build/${pathname.split("/")[2]}/gear`}
+      className={cn(
+        "h-full justify-start items-center flex px-6",
+        pathname.endsWith("/gear") 
+          ? "border-b-2 border-b-foreground hover:border-b-2 hover:border-b-foreground/50"
+          : "border-b-2 border-b-background/25 hover:border-b-2 hover:border-b-foreground"
+      )}
+    >
+      <Image
+        src="/icons/gear-logo.webp"
+        alt="Class Logo"
+        width={32}
+        height={32}
+      />
+    </Link>
+  );
+};
