3681fb056dd87ced4faa366db33e333d74bce495
Pamacea <pamacea@live.fr>
2025-12-16 14:03:55 +0100

    🔧 Configure database connection and migrations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "generated/prisma/client";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.BAHION_POSTGRES_URL || 
+                    process.env.BAHIONDB_PRISMA_DATABASE_URL || 
+                    process.env.DATABASE_URL || 
+                    "",
 });
 
 export const prisma = new PrismaClient({ adapter });
