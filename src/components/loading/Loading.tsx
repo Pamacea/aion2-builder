3fb5f665c9c7a247be19c1e3d6bbc4f461be850f
Pamacea <pamacea@live.fr>
2025-12-18 21:20:32 +0100

    🚑 Loading.tsx block the build

--- src/components/loading/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export const Loading = () => {
-  return (
-    <div className="flex items-center justify-center h-full w-full">
-      <div className="w-24 h-24 border-4 border-secondary/20 border-t-secondary rounded-full animate-spin" />
-    </div>
-  );
-};
